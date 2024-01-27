f5f747bc135d4891d5be24f8469b8b34b526811f
aniqr11 <kikijak482@gmail.com>
2024-01-27 08:23:04 +0700

    progress 1

---
+++ components/index.ts
@@ -0,0 +1,8 @@
+import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
+
+export { Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter };
